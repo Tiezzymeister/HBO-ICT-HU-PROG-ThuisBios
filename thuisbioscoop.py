56605153b455465b04019d3b344fa5897fc939d9
Tiezzynator <tiezzynator@gmail.com>
2016-11-10 01:29:00 +0100

    Maakt nu gebruik van de API en ik heb hem ff titel en regisseur laten printen om te kijken of het 100% werkt

--- thuisbioscoop.py
+++ thuisbioscoop.py
@@ -1,3 +1,6 @@
+import requests
+import xmltodict
+import datetime
 from tkinter import *
 root = Tk()
 
@@ -5,6 +8,23 @@ label = Label(master=root, text='Test', background='pink')
 label.pack()
 
 root.mainloop()
+
+
+def get_films():
+    api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=4520nc22kzoks8g1nbi4lihxyuu6z0ng&dag=0-0-0&sorteer=0'
+    api_url = list(api_url)
+    now = datetime.datetime.today()
+    api_url[83] = now.strftime("%d")
+    api_url[85] = now.strftime("%m")
+    api_url[87] = now.strftime("%Y")
+    api_url = "".join(str(item) for item in api_url)
+    response = requests.get(api_url)
+    xmldictionary = xmltodict.parse(response.text)
+    films = xmldictionary["filmsoptv"]["film"]
+    print(films)
+    for film in films:
+        if film["titel"] and film["regisseur"] is not None:
+            print(film["titel"] + " - " + film["regisseur"])
 
 def menu():
     print("Welkom, dit zijn uw opties: ")
@@ -20,7 +40,6 @@ def menu():
         totalen()
     else:
         menu()
-
 def aanmelden():
     print("Hier komt nog wat.")
 
@@ -30,4 +49,5 @@ def allefilms():
 def totalen():
     print("En ook nog wat hier...")
 
+get_films()
 menu()
